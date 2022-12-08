9a165bec67fc3f835bb402a8138a50ec49838728
pykan1 <vitalikmoskov233@gmail.com>
2022-12-08 13:53:44 +0300

    сфыс

---
+++ src/components/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список пользователей</h3>
+    <post-item
+        :post="post"
+        v-for="post in posts"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+    />
+  </div>
+  <h2 v-else style="color: red">
+    Список пуст
+  </h2>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem.vue";
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
